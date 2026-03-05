08f3f3f4c3aa86f6032a1cfd92d2f9b6bfe498be
filez34 <sophiakotova@gmail.com>
2026-03-05 11:32:20 +0100

    adding functionality

--- js/catalogue.js
+++ js/catalogue.js
@@ -1,4 +1,4 @@
 //  headingElement.style.display = "none";
-const headerElement = document.querySelector("header");
-const navElement = document.createElement("nav");
-headerElementElement.appendChild(navElement);
+// const headerElement = document.querySelector("header");
+// const navElement = document.createElement("nav");
+// headerElementElement.appendChild(navElement);
